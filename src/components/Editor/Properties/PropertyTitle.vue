1ff125b5ff3b5af113047e9041c2906c616c7de1
rita_gama <rita.gama@tecnico.ulisboa.pt>
2023-06-09 14:30:49 +0100

    Add emoji picker #4716
    Signed-off-by: rita_gama <rita.gama@tecnico.ulisboa.pt>
    Co-authored-by: joaquimlbacao-ist <joaquimluzbacao@tecnico.ulisboa.pt>

--- src/components/Editor/Properties/PropertyTitle.vue
+++ src/components/Editor/Properties/PropertyTitle.vue
@@ -19,30 +19,69 @@
   - along with this program. If not, see <http://www.gnu.org/licenses/>.
   -
   -->
-
 <template>
-	<div class="property-title">
-		<div class="property-title__input"
-			:class="{ 'property-title__input--readonly': isReadOnly }">
-			<input v-if="!isReadOnly"
-				v-focus
-				type="text"
-				:placeholder="t('calendar', 'Event title')"
-				:value="value"
-				@input.prevent.stop="changeValue">
-			<!-- eslint-disable-next-line vue/singleline-html-element-content-newline -->
-			<div v-else>{{ value }}</div>
+<div class="property-title">
+	<emoji-picker @emoji="insert" :search="search">
+		<div slot="emoji-invoker" slot-scope="{ events: { click: clickEvent } }" @click.stop="clickEvent">
+			<button type="button" class="button">
+				<Emoji />
+			</button>
 		</div>
+		<div slot="emoji-picker" slot-scope="{ emojis, insert, display }">
+			<div>
+			<div>
+				<input type="text" v-model="search">
+			</div>
+			<div>
+				<div v-for="(emojiGroup, category) in emojis" :key="category">
+				<h5>{{ category }}</h5>
+				<div>
+					<span
+					v-for="(emoji, emojiName) in emojiGroup"
+					:key="emojiName"
+					@click="insert(emoji)"
+					:title="emojiName"
+					>{{ emoji }}</span>
+				</div>
+				</div>
+			</div>
+			</div>
+		</div>
+	</emoji-picker>
+
+	<div class="property-title__input"
+		:class="{ 'property-title__input--readonly': isReadOnly }">
+		<input v-if="!isReadOnly"
+			v-focus
+			type="text"
+			:placeholder="t('calendar', 'Event title')"
+			:value="value"
+			@input.prevent.stop="changeValue">
+		<!-- eslint-disable-next-line vue/singleline-html-element-content-newline -->
+		<div v-else>{{ value }}</div>
 	</div>
+</div>
 </template>
 
 <script>
 import focus from '../../../directives/focus.js'
+import Emoji from 'vue-material-design-icons/EmoticonOutline.vue'
+import { EmojiPicker } from 'vue-emoji-picker'
 
 export default {
 	name: 'PropertyTitle',
+	data() {
+		return {
+			input: '',
+			search: '',
+		}
+	},
 	directives: {
 		focus,
+	},
+	components: {
+		EmojiPicker,
+		Emoji,
 	},
 	props: {
 		isReadOnly: {
@@ -58,6 +97,24 @@ export default {
 		changeValue(event) {
 			this.$emit('update:value', event.target.value)
 		},
+		insert(emoji) {
+			this.value += emoji;
+		},
 	},
 }
 </script>
+
+<style lang="scss" scoped>
+.button {
+background-color: white;
+border-style: solid;
+color: black;
+padding: 6px 6px;
+text-align: center;
+text-decoration: none;
+display: inline-block;
+font-size: 16px;
+border-radius: 12px;
+}
+</style>
+
